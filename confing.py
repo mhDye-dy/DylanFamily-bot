46cadd32524cb8ede9b6978647f1a1b66817e17f
mhDye-dy <ma09mhdye@gmail.com>
2025-07-26 17:31:27 +0330

    Create confing.py

---
+++ confing.py
@@ -0,0 +1,13 @@
+# config.py
+
+BOT_TOKEN = "6750126776:AAHAt1M9XRob0pLvnAQSUwyYI7K8X896YeQ"
+
+ADMINS = [726061852, 2048610084]
+
+REQUIRED_CHANNELS = [
+    "@dylanwang_iran",
+    "@estheryu_ir",
+    "@chinatime_ir"
+]
+
+WELCOME_MESSAGE = "به ربات DylanFamily خوش اومدی 🌸"
